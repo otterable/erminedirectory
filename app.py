bd1620928cd63728e004278d3aaa996ea07849a7
otterable <110373485+otterable@users.noreply.github.com>
2024-06-10 18:38:09 +0200

    a

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, redirect, url_for
 import os
 import time
-import re  # Import regex module
+import re
+import json
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -30,21 +31,18 @@ def get_images_with_categories(art_categories, category='all', sort='date-desc')
                 if filename.lower().endswith(('.jpg', '.png', '.gif')):
                     file_path = os.path.join(root, filename)
                     creation_time = os.path.getctime(file_path)
-                    # Extract numeric prefix
                     match = re.match(r"(\d+)", filename)
                     if match:
                         prefix = int(match.group(1))
                     else:
-                        prefix = float('inf')  # Ensure files without a prefix are sorted last
+                        prefix = float('inf')
                     all_images.append({
                         'filename': filename,
                         'category': cat,
                         'ctime': creation_time,
-                        'prefix': prefix  # Add prefix for sorting
+                        'prefix': prefix
                     })
-    # Sort by numeric prefix first, then by the specified sort order
     all_images.sort(key=lambda x: (x['prefix'], -x['ctime'] if sort == 'date-desc' else x['ctime']))
-    
     return all_images
 
 @app.route('/')
@@ -53,7 +51,6 @@ def index():
     images = get_images_with_categories(art_categories)
     return render_template('index.html', art_categories=art_categories, images=images)
 
-
 @app.route('/portfolio/', defaults={'category': 'all'})
 @app.route('/portfolio/<category>/')
 def portfolio(category):
@@ -61,7 +58,30 @@ def portfolio(category):
     images = get_images_with_categories(art_categories, category)
     return render_template('portfolio.html', art_categories=art_categories, images=images, selected_category=category)
 
+@app.route('/laternen')
+def laternen():
+    visit_count = increment_visit('visits.js')
+    redirect_url = 'https://www.etsy.com/listing/1717112463/'
+    return redirect(redirect_url)
 
+@app.route('/laternenqr')
+def laternenqr():
+    visit_count = increment_visit('visitsqr.js')
+    redirect_url = 'https://www.etsy.com/listing/1717112463/'
+    return redirect(redirect_url)
+
+def increment_visit(log_filename):
+    log_path = os.path.join(app.static_folder, log_filename)
+    if not os.path.exists(log_path):
+        visits = {'count': 0}
+    else:
+        with open(log_path, 'r') as file:
+            visits = json.load(file)
+    visits['count'] += 1
+    visits['time'] = time.strftime("%Y-%m-%d %H:%M:%S")
+    with open(log_path, 'w') as file:
+        json.dump(visits, file)
+    return visits['count']
 
 if __name__ == '__main__':
     app.run(debug=True)
